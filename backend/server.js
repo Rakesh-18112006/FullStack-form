0709a5c7fedf33704d67f075a0fd430f4468d3ca
Rakesh Mummana <mummanarakesh@gmail.com>
2025-07-26 22:57:25 +0530

    local to production

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true
+  origin: '*',           // 👈 allow all domains
+  credentials: false     // 👈 set to false if not using cookies
 }));
+
 app.use(bodyParser.json());
 
 // Database connection
@@ -31,7 +32,7 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
